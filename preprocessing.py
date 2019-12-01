79bd45281abf9c26352acf964f4bbea032edae6f
Wojtasek <wojtasek@Wojciechs-MacBook-Pro.local>
2019-12-01 23:45:48 +0100

    Add preprocessing, requirements

---
+++ preprocessing.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+def downcast_dtypes(df):
+    float_cols = [c for c in df if df[c].dtype == "float64"]
+    int_cols = [c for c in df if df[c].dtype in ["int64", "int32"]]
+    df[float_cols] = df[float_cols].astype(np.float32)
+    df[int_cols] = df[int_cols].astype(np.int16)
+    return df
